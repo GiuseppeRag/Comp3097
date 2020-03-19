bc43b773e4a25d9cf9bb40cec84418820081f4c0
GiuseppeRag <47995126+GiuseppeRag@users.noreply.github.com>
2020-03-19 15:22:53 -0400

    added week 11

---
+++ Week 11/Week 11/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Week 11
+//
+//  Created by user162289 on 3/19/20.
+//  Copyright © 2020 user162289. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func TextField(_ sender: Any) {
+    }
+    
+    @IBAction func MyButton(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var img1: UIImageView!
+}
+
---
+++ Week 11/Week 11Tests/Week_11Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Week_11Tests.swift
+//  Week 11Tests
+//
+//  Created by user162289 on 3/19/20.
+//  Copyright © 2020 user162289. All rights reserved.
+//
+
+import XCTest
+@testable import Week_11
+
+class Week_11Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
